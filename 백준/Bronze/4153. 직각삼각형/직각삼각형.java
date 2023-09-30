cc55bad49efaee0deeb05bbf6ee726318b018db1
허동민 <131800093+HurDong@users.noreply.github.com>
2023-10-01 03:10:28 +0900

    [Bronze III] Title: 직각삼각형, Time: 216 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            if (a == 0 && b == 0 && c == 0) {
+                break;
+            } else {
+                if (a >= b && a >= c) {
+                    if (a * a == b * b + c * c) {
+                        System.out.println("right");
+                    } else {
+                        System.out.println("wrong");
+                    }
+                } else if (b >= a && b >= c) {
+                    if (b * b == a * a + c * c) {
+                        System.out.println("right");
+                    } else {
+                        System.out.println("wrong");
+                    }
+                } else if (c >= a && c >= b) {
+                    if (c * c == b * b + a * a) {
+                        System.out.println("right");
+                    } else {
+                        System.out.println("wrong");
+                    }
+                }
+            }
+        }
+    }
+}

b868b36b3c2ff4f88bcacc821be439b829574132
허동민 <131800093+HurDong@users.noreply.github.com>
2023-10-01 15:28:52 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 2068 ms, Memory: 149496 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        Map<String, String> site = new HashMap<>();
+        String[] target = new String[m];
+        for (int i = 0; i < n; i++) {
+            site.put(sc.next(), sc.next());
+        }
+        for (int i = 0; i < m; i++) {
+            target[i] = sc.next();
+            System.out.println(site.get(target[i]));
+        }
+    }
+}

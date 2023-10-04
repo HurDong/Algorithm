962c36d7fab4ac0e03642ddd33f4ab32736e0b81
허동민 <131800093+HurDong@users.noreply.github.com>
2023-10-04 23:52:43 +0900

    [Bronze II] Title: Hashing, Time: 212 ms, Memory: 17724 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -5,9 +5,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String c = sc.next();
-        int sum = 0;
+        long sum = 0;
+        long temp = 1;
+        int M = 1234567891;
         for (int i = 0; i < n; i++) {
-            sum += (c.charAt(i) - 96) * Math.pow(31, i);
+            sum = (sum + (c.charAt(i) - 96) * temp) % M;
+            temp = (temp * 31) % M;
         }
         System.out.println(sum);
     }

58c56a63fe7fde44eff2a7f22e9eef90261c8f7c
허동민 <131800093+HurDong@users.noreply.github.com>
2023-10-05 00:18:26 +0900

    [Silver IV] Title: solved.ac, Time: 336 ms, Memory: 37884 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int avgN = (int) Math.round(n * 0.15);
+        int sum = 0;
+        int avg = 0;
+        int[] rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            rank[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(rank);
+        for (int i = avgN; i < n - avgN; i++) {
+            sum += rank[i];
+        }
+        int num = n - (2 * avgN);
+        avg = (int) Math.round((double) sum / num);
+        System.out.println(avg);
+    }
+}

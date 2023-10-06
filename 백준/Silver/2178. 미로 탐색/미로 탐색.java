cd90014c7b4de9489a36ec49475b12d05f79ca53
허동민 <131800093+HurDong@users.noreply.github.com>
2023-10-07 04:09:39 +0900

    [Silver I] Title: 미로 탐색, Time: 252 ms, Memory: 19024 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] maze = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            for (int j = 0; j < m; j++) {
+                maze[i][j] = s.charAt(j) - '0';
+            }
+        }
+        int[] dx = { -1, 1, 0, 0 };
+        int[] dy = { 0, 0, -1, 1 };
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] { 0, 0 });
+        visited[0][0] = true;
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    if (maze[nx][ny] == 1 && !visited[nx][ny]) {
+                        queue.add(new int[] { nx, ny });
+                        visited[nx][ny] = true;
+                        maze[nx][ny] = maze[cur[0]][cur[1]] + 1;
+                    }
+                }
+            }
+        }
+        System.out.println(maze[n - 1][m - 1]);
+    }
+}

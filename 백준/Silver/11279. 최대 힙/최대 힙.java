1a4df220f7b8cb83dca91ff8eabac99dc74bf338
허동민 <131800093+HurDong@users.noreply.github.com>
2023-10-08 21:19:58 +0900

    [Silver II] Title: 최대 힙, Time: 352 ms, Memory: 27220 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                if (queue.isEmpty()) {
+                    sb.append("0\n");
+                } else {
+                    sb.append(queue.poll() + "\n");
+                }
+            } else {
+                queue.add(num);
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}

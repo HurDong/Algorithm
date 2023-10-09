c4799d7ce1a9fe4915cdb1162faadf6c5a941a6f
허동민 <131800093+HurDong@users.noreply.github.com>
2023-10-10 00:21:35 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a=1, b=1;
+	while (a=!0,b=!0) {
+		cin >> a;
+		cin >> b;
+		if (a == 0, b == 0)
+			break;
+		cout << a + b << "\n";
+	}
+	return 0;
+}

6275f91cb3713baab63a397bc64bd1f67ddd9eb3
허동민 <131800093+HurDong@users.noreply.github.com>
2023-10-10 00:26:41 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 248 ms, Memory: 27784 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,30 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        while (queue.size() > 1) {
+            for (int i = 0; i < K - 1; i++) {
+                queue.add(queue.poll());
+            }
+            sb.append(queue.poll()).append(", ");
+        }
+
+        sb.append(queue.poll()).append(">");
+        System.out.println(sb);
+    }
+}

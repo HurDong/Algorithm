d53a11331fec08c3b3d97b1810d33f0c4e786ee2
허동민 <131800093+HurDong@users.noreply.github.com>
2023-10-10 00:02:42 +0900

    [Silver III] Title: 수열, Time: 1304 ms, Memory: 29172 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(st.nextToken());
+    int k = Integer.parseInt(st.nextToken());
+    int[] num = new int[n];
+    int[] sum = new int[n - k + 1];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      num[i] = Integer.parseInt(st.nextToken());
+    }
+    for (int i = 0; i < n - k + 1; i++) {
+      for (int j = 0; j < k; j++) {
+        sum[i] += num[i + j];
+      }
+    }
+    Arrays.sort(sum);
+    System.out.println(sum[n - k]);
+  }
+}

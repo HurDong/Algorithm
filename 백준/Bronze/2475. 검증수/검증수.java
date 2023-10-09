8c8b1b8263d24943caed6be0d11f3978a8c598c1
허동민 <131800093+HurDong@users.noreply.github.com>
2023-10-10 00:01:41 +0900

    [Bronze V] Title: 검증수, Time: 212 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] num = new int[5];
+        int[] numSquare = new int[5];
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            num[i] = sc.nextInt();
+            numSquare[i] = num[i] * num[i];
+            sum += numSquare[i];
+        }
+        System.out.println(sum % 10);
+    }
+}

6036e0fcb1e50c81e98451c4dcd460a033b28c63
허동민 <131800093+HurDong@users.noreply.github.com>
2023-10-10 00:23:43 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		long n1 = sc.nextLong();
+		long n2 = sc.nextLong();
+		long n3 = sc.nextLong();
+		long result = n1+n2+n3;
+		System.out.println(result);
+	}
+
+}

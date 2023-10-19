ff4739a4b23a3dd01a765ba8092e37f2bb58cf50
HurDong <gjehdals456@gmail.com>
2023-10-19 21:06:10 +0900

    commit 1019 2106

--- BOJ/b1260/Main.java
+++ BOJ/b1260/Main.java
@@ -3,21 +3,70 @@ package b1260;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int v = Integer.parseInt(st.nextToken());
-        Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < m; i++) {
+
+        int N = Integer.parseInt(st.nextToken()); // 정점의 개수
+        int M = Integer.parseInt(st.nextToken()); // 간선의 개수
+        int V = Integer.parseInt(st.nextToken()); // 시작 정점 번호
+
+        ArrayList<Integer>[] graph = new ArrayList[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        // 방문 정점 여러 개일때, 번호 작은 것부터 가기 위해 설정!
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        boolean[] visited = new boolean[N + 1];
+        dfs(graph, V, visited);
+        System.out.println();
+
+        Arrays.fill(visited, false); // bfs 실행을 위한 visited false 초기화
+        bfs(graph, V, visited);
+    }
+
+    public static void dfs(ArrayList<Integer>[] graph, int v, boolean[] visited) {
+        visited[v] = true;
+        System.out.print(v + " ");
+
+        for (int next : graph[v]) {
+            if (!visited[next]) {
+                dfs(graph, next, visited);
+            }
+        }
+    }
+
+    public static void bfs(ArrayList<Integer>[] graph, int start, boolean[] visited) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int v = queue.poll();
+            System.out.print(v + " ");
+
+            for (int next : graph[v]) {
+                if (!visited[next]) {
+                    queue.add(next);
+                    visited[next] = true;
+                }
+            }
         }
     }
 }
---
+++ BOJ/b2805/Main.java
@@ -0,0 +1,20 @@
+package b2805;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] tree = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            tree[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+}

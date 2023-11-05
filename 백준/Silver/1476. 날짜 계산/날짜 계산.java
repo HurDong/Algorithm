90657c07dfaa6eb2407f1b9a8fbf60d82368de02
허동민 <131800093+HurDong@users.noreply.github.com>
2023-11-05 15:55:56 +0900

    [Silver V] Title: 날짜 계산, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int E = sc.nextInt();
+        int S = sc.nextInt();
+        int M = sc.nextInt();
+
+        int year = 1;
+        while (true) {
+            if ((year - E) % 15 == 0 && (year - S) % 28 == 0 && (year - M) % 19 == 0) {
+                break;
+            }
+            year++;
+        }
+
+        System.out.println(year);
+    }
+}

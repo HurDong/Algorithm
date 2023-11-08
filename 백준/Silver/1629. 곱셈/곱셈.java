f94074ebc9968a9b477ea07fd63d55c82583fa0a
허동민 <131800093+HurDong@users.noreply.github.com>
2023-11-08 21:13:04 +0900

    [Silver I] Title: 곱셈, Time: 204 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+        System.out.println(multiply(A, B, C));
+    }
+
+    private static long multiply(long a, long b, long c) {
+        if (b == 0) {
+            return 1;
+        }
+        long temp = multiply(a, b / 2, c);
+        temp = (temp * temp) % c;
+        // A^4n = (A^2n)^2 = ((A^n)^2)^2 = ... 
+        if (b % 2 == 0) {
+            return temp;
+        }
+        return (temp * a) % c;
+    }
+}

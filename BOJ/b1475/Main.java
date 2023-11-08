afb280bbc16a198d72db1be95b47606e340401af
HurDong <gjehdals456@gmail.com>
2023-11-09 07:06:46 +0900

    commit 1109 0706

---
+++ BOJ/b1475/Main.java
@@ -0,0 +1,29 @@
+package b1475;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String roomNumber = br.readLine();
+        int[] count = new int[10];
+
+        for (int i = 0; i < roomNumber.length(); i++) {
+            count[roomNumber.charAt(i) - '0']++;
+        }
+
+        int maxSet = 0;
+        for (int i = 0; i < 10; i++) {
+            if (i != 6 && i != 9) {
+                maxSet = Math.max(maxSet, count[i]);
+            }
+        }
+
+        int sixAndNine = (count[6] + count[9] + 1) / 2;
+        maxSet = Math.max(maxSet, sixAndNine);
+
+        System.out.println(maxSet);
+    }
+}
---
+++ BOJ/b2644/Main.java
@@ -0,0 +1,56 @@
+package b2644;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    static int[][] family;
+    static boolean[] visited;
+    static int n;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int person1 = sc.nextInt();
+        int person2 = sc.nextInt();
+        int m = sc.nextInt();
+
+        family = new int[n + 1][n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            family[x][y] = family[y][x] = 1;
+        }
+
+        System.out.println(bfs(person1, person2));
+    }
+
+    static int bfs(int start, int end) {
+        Queue<Integer> q = new LinkedList<>();
+        int[] degree = new int[n + 1];
+
+        q.offer(start);
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+            int current = q.poll();
+
+            if (current == end) {
+                return degree[current];
+            }
+
+            for (int i = 1; i <= n; i++) {
+                if (family[current][i] == 1 && !visited[i]) {
+                    q.offer(i);
+                    visited[i] = true;
+                    degree[i] = degree[current] + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}

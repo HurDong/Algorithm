a4ab18c855ccaa5dba6f13dda0ddf1858bc2aaab
HurDong <gjehdals456@gmail.com>
2023-11-12 19:06:18 +0900

    commit 1112 1906

---
+++ BOJ/b1789/Main.java
@@ -0,0 +1,17 @@
+package b1789;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long sum = 0;
+        long count = 0;
+        for (int i = 1; sum <= n; i++) {
+            sum += i;
+            count++;
+        }
+        System.out.println(count - 1);
+    }
+}

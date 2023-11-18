81483f6462ed26980d7d936f4694669f4b1f380f
HurDong <gjehdals456@gmail.com>
2023-11-18 09:44:33 +0900

    commit 1118 0944

---
+++ BOJ/b11004/Main.java
@@ -0,0 +1,28 @@
+package b11004;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr); // 오름차순 정렬
+
+        System.out.println(arr[K - 1]);
+    }
+}
---
+++ BOJ/b1439/Main.java
@@ -0,0 +1,25 @@
+package b1439;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int oneSeq = 0, zeroSeq = 0;
+        if (s.charAt(0) == '0')
+            zeroSeq++;
+        else
+            oneSeq++;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) != s.charAt(i + 1)) {
+                if (s.charAt(i + 1) == '0') {
+                    zeroSeq++;
+                } else {
+                    oneSeq++;
+                }
+            }
+        }
+        System.out.println(Math.min(oneSeq, zeroSeq));
+    }
+}

f620cc575a0628c95a59d7408ec447a543c137a4
HurDong <gjehdals456@gmail.com>
2023-11-19 01:28:04 +0900

    commit 1119 0128

--- BOJ/b7662/Main.java
+++ BOJ/b7662/Main.java
@@ -3,47 +3,47 @@ package b7662;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
 
         while (T-- > 0) {
             int k = Integer.parseInt(br.readLine());
-            PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-            PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> o2 - o1);
-
+            TreeMap<Integer, Integer> map = new TreeMap<>();
             for (int i = 0; i < k; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 String command = st.nextToken();
                 int value = Integer.parseInt(st.nextToken());
 
                 if ("I".equals(command)) {
-                    minHeap.offer(value);
-                    maxHeap.offer(value);
+                    // value가 key값으로 있으면 value값을 1증가 없으면 1로 설정
+                    map.put(value, map.getOrDefault(value, 0) + 1);
                 } else {
-                    if (value == 1) {
-                        if (!maxHeap.isEmpty()) {
-                            int max = maxHeap.poll();
-                            minHeap.remove(max);
+                    if (!map.isEmpty()) {
+                        int key;
+                        if (value == 1) {
+                            key = map.lastKey();
+                        } else {
+                            key = map.firstKey();
                         }
-                    } else {
-                        if (!minHeap.isEmpty()) {
-                            int min = minHeap.poll();
-                            maxHeap.remove(min);
+                        if (map.put(key, map.get(key) - 1) == 1) {
+                            map.remove(key); // count값이 0이되면 삭제
                         }
                     }
                 }
-            }
 
-            if (minHeap.isEmpty() || maxHeap.isEmpty()) {
-                System.out.println("EMPTY");
+            }
+            if (map.isEmpty()) {
+                sb.append("EMPTY\n");
             } else {
-                System.out.println(maxHeap.poll() + " " + minHeap.poll());
+                sb.append(map.lastKey() + " " + map.firstKey() + "\n");
             }
         }
+        System.out.println(sb.toString());
     }
 }

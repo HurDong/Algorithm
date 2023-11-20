6d9c1431bf1fa69f18f9fac9fb3012f27efba39b
HurDong <gjehdals456@gmail.com>
2023-11-20 18:15:27 +0900

    commit 1120 1815

---
+++ BOJ/b11725/Main.java
@@ -0,0 +1,7 @@
+package b11725;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ BOJ/b2167/Main.java
@@ -0,0 +1,34 @@
+package b2167;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[][] arr = new int[N + 1][M + 1];
+        int[][] prefixSum = new int[N + 1][M + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                arr[i][j] = sc.nextInt();
+                prefixSum[i][j] = arr[i][j] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
+            }
+        }
+
+        int K = sc.nextInt();
+        for (int k = 0; k < K; k++) {
+            int i = sc.nextInt();
+            int j = sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            int sum = prefixSum[x][y] - prefixSum[i - 1][y] - prefixSum[x][j - 1] + prefixSum[i - 1][j - 1];
+            sb.append(sum + "\n");
+        }
+        System.out.print(sb.toString());
+    }
+
+}

807a5acac4cacd2de1326c0e0e3fdf1b2a05d9e8
허동민 <131800093+HurDong@users.noreply.github.com>
2023-11-23 01:30:41 +0900

    [Gold V] Title: 평범한 배낭, Time: 292 ms, Memory: 57536 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[][] dp = new int[n + 1][k + 1];
+        int[] weights = new int[n + 1];
+        int[] values = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            weights[i] = sc.nextInt();
+            values[i] = sc.nextInt();
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                if (weights[i] <= j) { // 먼저 i번째 물품이 dp[i][j]에 포함될 수 있는 지 확인
+                    dp[i][j] = Math.max(dp[i - 1][j], values[i] + dp[i - 1][j - weights[i]]);
+                } else { // 포함되지 않는다면 그대로 진행
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        System.out.println(dp[n][k]);
+    }
+}

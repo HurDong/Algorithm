d01d61c2f8875690d8ff07a658ec4e01eb4310bc
허동민 <131800093+HurDong@users.noreply.github.com>
2023-11-28 19:01:40 +0900

    [Silver III] Title: 이친수, Time: 212 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[] dp = new long[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+            // i-1가 0으로 끝난 경우 : dp[i-1]인 경우
+            // i-1가 1으로 끝난 경우 : dp[i-2] + '1' 추가한 경우
+        }
+        System.out.println(dp[n]);
+    }
+}

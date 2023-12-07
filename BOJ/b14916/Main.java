579f6662c622122f54d9d491848bc5be9cd16785
HurDong <gjehdals456@gmail.com>
2023-12-07 23:32:42 +0900

    commit 1207 2332

---
+++ BOJ/b14916/Main.java
@@ -0,0 +1,25 @@
+package b14916;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        while (n > 0) {
+            if (n % 5 == 0) {
+                count += n / 5;
+                break;
+            } else {
+                n -= 2;
+                count++;
+            }
+        }
+        if (n < 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(count);
+        }
+    }
+}

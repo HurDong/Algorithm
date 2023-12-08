b877f01d163fd0339a17bcedbcac93af8ac684b8
허동민 <131800093+HurDong@users.noreply.github.com>
2023-12-08 21:40:26 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 1024 ms, Memory: 110396 KB -BaekjoonHub

---
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(sc.nextInt());
+        }
+        Integer[] nums = set.toArray(new Integer[0]);
+
+        Arrays.sort(nums);
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}

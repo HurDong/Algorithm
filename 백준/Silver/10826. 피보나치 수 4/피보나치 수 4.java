1bb112c5efc621ccfe75ad2f2d76f363663bb5d3
허동민 <131800093+HurDong@users.noreply.github.com>
2023-12-16 15:59:28 +0900

    [Silver V] Title: 피보나치 수 4, Time: 260 ms, Memory: 24984 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // 큰 수 표현
+        BigInteger[] fib = new BigInteger[Math.max(n + 1, 2)];
+        fib[0] = BigInteger.ZERO;
+        fib[1] = BigInteger.ONE;
+
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1].add(fib[i - 2]);
+        }
+
+        System.out.println(fib[n]);
+    }
+}

2d7734cf25d554373da93b651389272978fe9224
허동민 <131800093+HurDong@users.noreply.github.com>
2023-12-18 22:10:52 +0900

    [Silver V] Title: 폴리오미노, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String board = sc.nextLine();
+        sc.close();
+
+        String result = replacePolynomino(board);
+        System.out.println(result);
+    }
+
+    private static String replacePolynomino(String board) {
+        StringBuilder sb = new StringBuilder();
+        int countX = 0;
+
+        for (int i = 0; i < board.length(); i++) {
+            if (board.charAt(i) == 'X') {
+                countX++;
+            } else {
+                if (countX % 2 != 0) {
+                    return "-1"; // 'X'의 개수가 홀수이면 불가능
+                }
+
+                sb.append(replaceWithPolynomino(countX));
+                sb.append('.'); // '.'을 그대로 추가
+                countX = 0; // 'X' 개수 초기화
+            }
+        }
+
+        if (countX % 2 != 0) {
+            return "-1";
+        }
+        sb.append(replaceWithPolynomino(countX));
+
+        return sb.toString();
+    }
+
+    private static String replaceWithPolynomino(int countX) {
+        StringBuilder sb = new StringBuilder();
+        while (countX >= 4) {
+            sb.append("AAAA");
+            countX -= 4;
+        }
+
+        if (countX == 2) {
+            sb.append("BB");
+        }
+        return sb.toString();
+    }
+}

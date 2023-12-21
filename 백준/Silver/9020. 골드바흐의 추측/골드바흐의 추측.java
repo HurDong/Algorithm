e2d1105294506a1a7473969d09e5cfeaeb62e7ac
허동민 <131800093+HurDong@users.noreply.github.com>
2023-12-21 23:04:01 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 644 ms, Memory: 27588 KB -BaekjoonHub

---
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int n1 = n / 2;
+            int n2 = n / 2;
+            while (true) {
+                if (isPrime(n1) && isPrime(n2)) {
+                    System.out.println(n1 + " " + n2);
+                    break;
+                }
+                n1--;
+                n2++;
+            }
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        if (num < 2)
+            return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+}

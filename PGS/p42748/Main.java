66885e5ffb75d75189860e6770e325a97e4eaa70
허동민 <131800093+HurDong@users.noreply.github.com>
2024-01-05 13:57:42 +0900

    Create Main.java

---
+++ PGS/p42748/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int T = commands.length;
+        int idx = 0;
+        while(T-->0){
+            int[] arr = new int[commands[idx][1] - commands[idx][0]+1];
+            int arrIdx =0 ;
+            for(int i=commands[idx][0]-1;i<commands[idx][1];i++)
+            {
+                arr[arrIdx] = array[i];
+                arrIdx++;
+            }
+            Arrays.sort(arr);
+            list.add(arr[commands[idx][2]-1]);
+            idx++;
+        }
+        System.out.println(list);
+        int[] answer = new int[list.size()];
+        for(int i =0;i<answer.length;i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}

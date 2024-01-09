8a0947ee1ef43086acd88d2d77563ff9dd5f7fb3
허동민 <131800093+HurDong@users.noreply.github.com>
2024-01-09 21:28:16 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 228 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 난이도순으로 배치했지만 쉬운 난이도가 점수를 더 많는 경우가 있음.
+        // 점수 내리는 것을 최소한으로
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] score = new int[N];
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            score[i] = sc.nextInt();
+        }
+        for (int i = N - 1; i >= 1; i--) {
+            while (score[i] <= score[i - 1]) {
+                score[i - 1]--;
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}

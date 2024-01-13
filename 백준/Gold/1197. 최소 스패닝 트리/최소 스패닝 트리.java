7fcd6329c87e9f427b92aa0aa11b1274be31931b
허동민 <131800093+HurDong@users.noreply.github.com>
2024-01-13 15:29:09 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 636 ms, Memory: 48812 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int v, e;
+    static int[] parents;
+    static ArrayList<Node> nodes = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        parents = new int[v + 1];
+        // 자기자신으로 먼저 parents 초기화
+        for (int i = 1; i <= v; i++) {
+            parents[i] = i;
+        }
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            nodes.add(new Node(a, b, c));
+        }
+        Collections.sort(nodes);
+        int sum = 0;
+        for (Node node : nodes) {
+            if (find(node.start) != find(node.vertex)) {
+                union(node.start, node.vertex);
+                sum += node.weight;
+            }
+        }
+        System.out.println(sum);
+    }
+
+    private static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x != y) {
+            parents[y] = x;
+        }
+    }
+
+    private static int find(int x) {
+        if (parents[x] == x)
+            return x;
+        return parents[x] = find(parents[x]);
+    }
+
+    public static class Node implements Comparable<Node> {
+        int start;
+        int vertex;
+        int weight;
+
+        public Node(int start, int vertex, int weight) {
+            this.start = start;
+            this.vertex = vertex;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.weight - o.weight; // 오름차순
+        }
+    }
+}

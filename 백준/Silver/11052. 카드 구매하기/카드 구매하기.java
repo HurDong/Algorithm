4e7eb3822980653c6d05571eace13b763f258886
허동민 <131800093+HurDong@users.noreply.github.com>
2024-01-19 11:31:50 +0900

    [Silver I] Title: 카드 구매하기, Time: 300 ms, Memory: 20904 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// 1~n개가 들어있는 카드팩 -> 최대 비용
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		int[] cards = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			cards[i] = sc.nextInt();
+		}
+
+		// dp[i] : i개의 카드가 필요한 경우
+		int[] dp = new int[n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			dp[i] = cards[i];
+			for (int j = 1; j < i; j++) {
+				dp[i] = Math.max(dp[i - j] + cards[j], dp[i]);
+			}
+		}
+		System.out.println(dp[n]);
+	}
+}

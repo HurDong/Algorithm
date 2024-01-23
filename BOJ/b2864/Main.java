cd4174c2285d32951c18acef6a56e2dd01a5c58f
허동민 <gjehdals456@gmail.com>
2024-01-23 17:52:43 +0900

    commit 0123 1752

---
+++ BOJ/b2864/Main.java
@@ -0,0 +1,28 @@
+package b2864;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String a = sc.next();
+		String b = sc.next();
+		String bigA = a, bigB = b, smallA = a, smallB = b;
+		// 5를 6으로 , 6을 5로 바꿀 수 있다. 최솟값 최댓값 구하기.
+		for (int i = 0; i < a.length(); i++) {
+			if (a.charAt(i) == '5')
+				bigA = a.replace('5', '6');
+			else if (a.charAt(i) == '6')
+				smallA = a.replace('6', '5');
+
+			if (b.charAt(i) == '5')
+				bigB = b.replace('5', '6');
+			else if (b.charAt(i) == '6')
+				smallB = b.replace('6', '5');
+
+		}
+		System.out.println((Integer.parseInt(smallA) + Integer.parseInt(smallB)) + " "
+				+ (Integer.parseInt(bigA) + Integer.parseInt(bigB)));
+	}
+}
---
+++ BOJ/b4796/Main.java
@@ -0,0 +1,27 @@
+package b4796;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		// 연속하는 20일중 10일 사용 가능
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int caseIdx = 1;
+
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int l = Integer.parseInt(st.nextToken()); // 연속 가능 일수
+			int p = Integer.parseInt(st.nextToken()); // 범위 일수
+			int v = Integer.parseInt(st.nextToken()); // 전체 일수
+			if (l == 0 && p == 0 && v == 0) {
+				return;
+			}
+			int answer = (v / p) * l + Math.min(l, (v % p));
+			System.out.println("Case " + caseIdx++ + ": " + answer);
+		}
+	}
+}

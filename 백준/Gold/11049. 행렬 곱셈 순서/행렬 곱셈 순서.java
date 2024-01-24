82f2424481a84b225a388afd3c2fd41be029f2a6
허동민 <131800093+HurDong@users.noreply.github.com>
2024-01-24 11:52:55 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 500 ms, Memory: 24004 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		ArrayList<int[]> list = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			list.add(new int[] { sc.nextInt(), sc.nextInt() });
+		}
+
+		// dp[i][j] : i번째 행렬부터 j번째 행렬까지 있을 때 최소 곱
+		int[][] dp = new int[n][n];
+
+		for (int k = 1; k < n; k++) {
+			for (int i = 0; i + k < n; i++) {
+				dp[i][i + k] = Integer.MAX_VALUE;
+				for (int j = i; j < i + k; j++) {
+					// dp[i][j]와 dp[j+1][i+k]의 합에 i번째 행렬의 행, j번째 행렬의 열, i+k번째 행렬의 열을 곱한 값을 더해준다
+					dp[i][i + k] = Math.min(dp[i][i + k],
+							dp[i][j] + dp[j + 1][i + k] + list.get(i)[0] * list.get(j)[1] * list.get(i + k)[1]);
+				}
+			}
+		}
+		System.out.println(dp[0][n - 1]);
+	}
+}

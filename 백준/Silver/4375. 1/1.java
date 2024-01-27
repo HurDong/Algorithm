a69b51c21e5ee62c93dc08efbbb386a0ebd33cea
허동민 <131800093+HurDong@users.noreply.github.com>
2024-01-27 22:48:43 +0900

    [Silver III] Title: 1, Time: 216 ms, Memory: 18196 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            int num = 0;
+            for (int i = 1;; i++) {
+                num = (num * 10) + 1;
+                num = num % n;
+                if (num == 0) {
+                    sb.append(i + "\n");
+                    break;
+                }
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}

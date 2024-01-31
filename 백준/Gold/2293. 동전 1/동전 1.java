2b514f11dac69bf2891e594437345380a33dc996
허동민 <131800093+HurDong@users.noreply.github.com>
2024-01-31 20:12:01 +0900

    [Gold V] Title: 동전 1, Time: 232 ms, Memory: 21876 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -22,12 +22,14 @@ public class Main {
 		// dp[i][j] : 1~i까지의 동전으로 j원 만드는 경우의 수
 		int[][] dp = new int[n + 1][k + 1];
 
+		// array indx out of bounds를 위해 idx를 설정해 해당 idx까지만 dp를 실시하도록한다.
 		int idx = n;
 
 		for (int i = 1; i <= n; i++) {
 			if (coins[i] <= k) {
 				dp[i][coins[i]] = 1;
 			} else {
+				// k보다 크면 의미가 없으므로 idx = i로 설정하고 break
 				idx = i;
 				break;
 			}
@@ -37,13 +39,15 @@ public class Main {
 			for (int j = 1; j <= k; j++) {
 				if (j > coins[i]) {
 					dp[i][j] += dp[i][j - coins[i]] + dp[i - 1][j];
-				} else {
+				}
+				// 지금 인덱스의 동전으로 dp[i-1][j-coins[i]]일때 만들 수 없는 경우 dp[i-1][j]만 가능
+				else {
 					dp[i][j] += dp[i - 1][j];
 				}
-				// System.out.println(i + " " + j + " " + coins[i] + " " + dp[i][j]);
 			}
 		}
 
+		// 1~idx번까지의 동전으로 k 원을 만드는 경우의 수
 		System.out.println(dp[idx][k]);
 	}
 

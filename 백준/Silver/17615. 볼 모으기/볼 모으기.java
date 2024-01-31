6f8890187b3d817a7f5b9bf5c1392d96ea890b73
허동민 <131800093+HurDong@users.noreply.github.com>
2024-01-31 10:21:01 +0900

    [Silver I] Title: 볼 모으기, Time: 504 ms, Memory: 30092 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.java
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.java
@@ -1,6 +1,30 @@
 import java.util.Scanner;
 
 public class Main {
+//	public static void main(String[] args) {
+//		// B와 R로 칠해진 볼 구성에 대하여 한가지 색의 볼만 옮길 때
+//		// 같은 색이 전부 모이는 최소 이동 횟수
+//		Scanner sc = new Scanner(System.in);
+//
+//		int n = sc.nextInt();
+//
+//		int answer = Integer.MAX_VALUE;
+//
+//		String s = sc.next();
+//
+//		// 처음 글자를 얻는다.
+//		char start = s.charAt(0);
+//
+//		// 마지막 글자를 얻는다.
+//		char end = s.charAt(n - 1);
+//
+//		// 앞에서 연속된
+//		for (int i = 0; i < n; i++) {
+//			
+//		}
+//
+//		System.out.println(answer);
+//	}
 	public static void main(String[] args) {
 		// B와 R로 칠해진 볼 구성에 대하여 한가지 색의 볼만 옮길 때
 		// 같은 색이 전부 모이는 최소 이동 횟수
@@ -28,92 +52,56 @@ public class Main {
 		// 두 색깔에 대하여 왼쪽 or 오른쪽으로 가는 경우의 수
 		int answer = Integer.MAX_VALUE;
 
-		// 1. 왼쪽으로
-		// 왼쪽으로 이동 시 왼쪽에 있는 것부터 왼쪽으로
-
-		// a. Red
 		int redLeftCount = 0;
+		int blueLeftCount = 0;
+		int redRightCount = 0;
+		int blueRightCount = 0;
+
+		if (redLeft[0] == 'B')
+			blueLeftCount++;
+		else
+			redLeftCount++;
 
 		for (int i = 1; i < n; i++) {
 			if (redLeft[i] == 'R') {
-				for (int j = 0; j < i; j++) {
-					// i의 앞쪽에서 다른 색깔이 있으면
-					if (redLeft[j] == 'B') {
-						char temp = redLeft[i];
-						redLeft[i] = redLeft[j];
-						redLeft[j] = temp;
-						redLeftCount++;
-						break;
-					}
+				if (blueLeftCount > 0) {
+					redLeftCount++;
+				}
+			} else {
+				if (redLeftCount > 0) {
+					blueLeftCount++;
 				}
 			}
 		}
 
-		answer = Math.min(answer, redLeftCount);
+		if (redLeft[0] == 'B')
+			blueLeftCount--;
+		else
+			redLeftCount--;
 
-		// b. Blue
-		int blueLeftCount = 0;
+		answer = Math.min(answer, Math.min(redLeftCount, blueLeftCount));
 
-		for (int i = 1; i < n; i++) {
-			if (blueLeft[i] == 'B') {
-				for (int j = 0; j < i; j++) {
-					// i의 앞쪽에서 다른 색깔이 있으면
-					if (redLeft[j] == 'R') {
-						char temp = blueLeft[i];
-						blueLeft[i] = blueLeft[j];
-						blueLeft[j] = temp;
-						blueLeftCount++;
-						break;
-					}
-				}
-			}
-		}
-
-		answer = Math.min(answer, blueLeftCount);
-
-		// 2. 오른쪽으로
-		// 오른쪽으로 이동 시 오른쪽에 있는 것부터 오른쪽으로
-
-		// a. Red
-		int redRightCount = 0;
+		if (redLeft[n - 1] == 'B')
+			blueRightCount++;
+		else
+			redRightCount++;
 
 		for (int i = n - 2; i >= 0; i--) {
 			if (redRight[i] == 'R') {
-				for (int j = n - 1; j > i; j--) {
-					// i의 앞쪽에서 다른 색깔이 있으면
-					if (redRight[j] == 'B') {
-						char temp = redRight[i];
-						redRight[i] = redRight[j];
-						redRight[j] = temp;
-						redRightCount++;
-						break;
-					}
+				if (blueRightCount > 0) {
+					redRightCount++;
+				}
+			} else {
+				if (redRightCount > 0) {
+					blueRightCount++;
 				}
 			}
 		}
-
-		answer = Math.min(answer, redRightCount);
-
-		// b. Blue
-		int blueRightCount = 0;
-
-		for (int i = n - 2; i >= 0; i--) {
-			if (blueRight[i] == 'B') {
-				for (int j = n - 1; j > i; j--) {
-					// i의 앞쪽에서 다른 색깔이 있으면
-					if (blueRight[j] == 'R') {
-						char temp = blueRight[i];
-						blueRight[i] = blueRight[j];
-						blueRight[j] = temp;
-						blueRightCount++;
-						break;
-					}
-				}
-			}
-		}
-
-		answer = Math.min(answer, blueRightCount);
-
+		if (redLeft[n - 1] == 'B')
+			blueRightCount--;
+		else
+			redRightCount--;
+		answer = Math.min(answer, Math.min(redRightCount, blueRightCount));
 		System.out.println(answer);
 	}
 }

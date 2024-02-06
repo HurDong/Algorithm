0ae327ab9e5f25c7db5562166f1197b6bcfd235a
허동민 <131800093+HurDong@users.noreply.github.com>
2024-02-06 17:51:15 +0900

    [Gold V] Title: A와 B, Time: 92 ms, Memory: 11452 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.java
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		String s = sc.next();
+		String s = br.readLine();
 
-		String rs = "";
-
-		String t = sc.next();
+		String t = br.readLine();
 
 		int n = s.length();
 

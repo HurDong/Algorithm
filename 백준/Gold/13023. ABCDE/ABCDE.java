0b2a715986e12930f322e576ade8212b7847e974
허동민 <131800093+HurDong@users.noreply.github.com>
2024-02-20 09:19:20 +0900

    [Gold V] Title: ABCDE, Time: 288 ms, Memory: 17048 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -26,6 +26,7 @@ public class Main {
 			friends.add(new ArrayList<>());
 		}
 
+		// 각자 ArrayList<>()에 서로를 추가
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 
@@ -38,20 +39,24 @@ public class Main {
 			friends.get(b).add(a);
 		}
 
+		// DFS를 돌며 5명이 문제의 조건을 만족하는 경우가 생기는 지 확인
 		for (int i = 0; i < N; i++) {
 			visited[i] = true;
 			dfs(friends, i, 0);
 			visited[i] = false;
 		}
 
+		// System.exit(0)을 기저 조건 실행문에 넣었으므로 이 부분에 오는 경우 0을 출력
 		System.out.println(0);
 	}
 
 	private static void dfs(ArrayList<ArrayList<Integer>> friends, int start, int count) {
+		// 문제의 조건에 맞을 때 1을 출력 후 System.exit(0)
 		if (count == 4) {
 			System.out.println(1);
 			System.exit(0);
 		}
+		// 이전 dfs의 start에 친구에 대하여 방문하지 않은 친구를 방문
 		for (int i = 0; i < friends.get(start).size(); i++) {
 			int num = friends.get(start).get(i);
 			if (!visited[num]) {

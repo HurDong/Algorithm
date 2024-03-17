dd0900507df6fbc8e1aaf9e2a0bd7356c016b1d2
허동민 <131800093+HurDong@users.noreply.github.com>
2024-03-17 18:44:28 +0900

    [Silver IV] Title: 정수 제곱근, Time: 80 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
@@ -5,9 +5,9 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        long n = Long.parseLong(br.readLine());
-        long answer = (long) Math.sqrt(n);
-        if (answer * answer < n) {
+        long n = Long.parseLong(br.readLine()); // 2^63 이므로 정수형 타입 long으로 설정
+        long answer = (long) Math.sqrt(n); // 제곱근을 사용 후 long으로 캐스팅해 소수점 버리기
+        if (answer * answer < n) { // n이 어떤 정수의 제곱근이 아닐 경우 answer을 더해줌
             answer++;
         }
         System.out.println(answer);

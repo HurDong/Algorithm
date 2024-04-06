ce3a5f83914816c3340e2ecd716fe55871e96bde
허동민 <131800093+HurDong@users.noreply.github.com>
2024-04-06 23:18:33 +0900

    [Silver IV] Title: 토너먼트, Time: 216 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int A = sc.nextInt();
+
+        int B = sc.nextInt();
+
+        int round = 0;
+
+        while (A != B) {
+            A = A / 2 + A % 2; // 김지민의 다음 라운드 번호
+            B = B / 2 + B % 2; // 임한수의 다음 라운드 번호
+            round++; // 라운드 증가
+        }
+
+        System.out.println(round);
+    }
+}

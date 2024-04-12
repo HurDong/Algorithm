9a58f8ba86f3e21462c1afea6596cc3c1cd3a5d2
허동민 <131800093+HurDong@users.noreply.github.com>
2024-04-12 10:20:54 +0900

    [Silver IV] Title: 좋은 단어, Time: 320 ms, Memory: 22164 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		int answer = 0;
+
+		for (int i = 0; i < n; i++) {
+			String s = br.readLine();
+			Stack<Character> stack = new Stack<>(); // 스택을 이용해서 문자를 저장하고 tail과 짝지어진다면
+			for (int j = 0; j < s.length(); j++) {
+				char ch = s.charAt(j);
+				// stack에 문자가 남아있으면
+				if (!stack.isEmpty()) {
+					// tail에 있는 문자와 같으면
+					if (stack.peek() == ch) {
+						// 짝지어지므로 pop
+						stack.pop();
+					}
+					// 다르다면 일단 stack에 쌓는다 교차하지만 않으면 되므로 진행
+					else {
+						stack.add(ch);
+					}
+				}
+				// 스택이 비어있으면 문자열을 add
+				else {
+					stack.add(ch);
+				}
+			}
+			// 스택이 비어있어야 좋은 단어다.
+			if (stack.isEmpty()) {
+				answer++;
+			}
+		}
+		System.out.println(answer);
+	}
+}

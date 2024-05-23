9e06c362ab1d8e9f43d14647107a6bee7371b32b
허동민 <131800093+HurDong@users.noreply.github.com>
2024-05-23 22:15:20 +0900

    [Bronze I] Title: 유레카 이론, Time: 336 ms, Memory: 21260 KB -BaekjoonHub

---
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        StringBuilder sb = new StringBuilder();
+
+        int T = sc.nextInt();
+
+        List<Integer> nums = new ArrayList<>();
+
+        nums.add(1);
+
+        nums.add(3);
+
+        while (nums.get(nums.size() - 1) <= 1000) {
+            nums.add(nums.get(nums.size() - 1) + nums.size() + 1);
+        }
+        List<Integer> e1 = new ArrayList<>();
+
+        List<int[]> e2 = new ArrayList<>();
+
+        List<int[]> e3 = new ArrayList<>();
+
+        List<Integer> eurekas = new ArrayList<>();
+
+        for (int i = 0; i < nums.size(); i++) {
+            int n1 = nums.get(i);
+            for (int j = 0; j <= i; j++) {
+                int n2 = nums.get(j);
+                for (int k = 0; k <= i; k++) {
+                    int n3 = nums.get(k);
+                    eurekas.add(n1 + n2 + n3);
+                }
+            }
+        }
+
+        // System.out.println(eurekas);
+
+        for (int i = 0; i < T; i++) {
+            int K = sc.nextInt();
+            sb.append(eurekas.contains(K) ? 1 : 0).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+}

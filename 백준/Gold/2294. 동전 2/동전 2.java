1ca28aa6819632a7a3c5545e5732065f1eadb0f6
허동민 <131800093+HurDong@users.noreply.github.com>
2024-06-20 18:17:40 +0900

    [Gold V] Title: 동전 2, Time: 240 ms, Memory: 17896 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int k = sc.nextInt();
+
+        int[] coins = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+        }
+
+        // coins 오름차순 정렬
+        Arrays.sort(coins);
+
+        int[] dp = new int[k + 1]; // dp[i] : i원을 만들 때 최소 개수
+
+        // 절대 도달못하는 값으로 설정
+        Arrays.fill(dp, k + 1);
+
+        dp[0] = 0;
+
+        // dp 로직 ...
+        for (int i = 1; i <= k; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i - coins[j] >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+
+        // dp[k]가 k+1 이면 못 만드는 경우
+        System.out.println(dp[k] > k ? -1 : dp[k]);
+    }
+}

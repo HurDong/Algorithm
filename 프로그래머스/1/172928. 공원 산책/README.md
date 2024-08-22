c1aecd2ae8dfaf03a4a7953bcf725e2882df2917
허동민 <131800093+HurDong@users.noreply.github.com>
2024-08-22 17:15:34 +0900

    [level 1] Title: 공원 산책, Time: 0.48 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/README.md
+++ 프로그래머스/1/172928. 공원 산책/README.md
@@ -1,10 +1,10 @@
 # [level 1] 공원 산책 - 172928 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928#) 
 
 ### 성능 요약
 
-메모리: 71.6 MB, 시간: 17.60 ms
+메모리: 76.6 MB, 시간: 0.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 22일 17:05:16
+2024년 08월 22일 17:15:31
 
 ### 문제 설명
 
--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -1,20 +1,28 @@
 import java.util.*;
 
 class Solution {
+    // 방향 벡터
     static int[] dx = { -1, 1, 0, 0 };
     static int[] dy = { 0, 0, -1, 1 };
-    static int h, w; // 높이 너비
+    // 높이 너비
+    static int h, w; 
     public int[] solution(String[] park, String[] routes) {
         h = park.length;
+        
         w = park[0].length();
+        
         int idx = 0;
-        boolean isStart = false;
+        // 시작 지점 찾기 위한 변수
+        boolean isStart = false; 
+        // 시작 지점 배열 -> x,y보다 밑에 좌표 반환 메서드에서 활용 용이
         int[] cur = new int[2];
         for(int i=0;i<h;i++)
         {
             for(int j=0;j<w;j++)
             {
+                // 시작 지점 찾으면
                 if(park[i].charAt(j)=='S'){
+                    // 찾았다고 알리고 변수 할당
                     isStart = true;
                     cur[0] = i;
                     cur[1] = j;
@@ -25,26 +33,32 @@ class Solution {
                 break;
             }
         }
+        // 명령어 수만큼 수행
         while(idx < routes.length){
-            int[] next = move(park,routes[idx],cur);
-            cur[0] = next[0];
-            cur[1] = next[1];
+            // 이동 수행
+            cur = move(park,routes[idx],cur);
+            // 다음 명령어로 이동
             idx++;
-            System.out.println(idx + " : " + cur[0] + " " + cur[1]);
         }
-        int[] answer = new int[2];
-        answer[0] = cur[0];
-        answer[1] = cur[1];
-        return answer;
+        // 정답 배열 선언
+        return new int[]{cur[0],cur[1]};
     }
+    // 이동 메서드
     public int[] move(String[] park,String route, int[] cur){
+        // 임시 배열
         int[] temp = new int[2];
+        // 깊은 복사
         temp[0] = cur[0];
         temp[1] = cur[1];
+        // 띄어쓰기 기준으로 String 나누기
         String[] splitRoute = route.split(" ");
+        // 방향(char)
         char dirChar = splitRoute[0].charAt(0);
+        // 거리
         int dist = splitRoute[1].charAt(0) - '0';
+        // 방향
         int dir = -1;
+        // dirChar 값에 따라 방향 실제 설정
         switch(dirChar){
             case 'E' :
                 dir = 3;
@@ -60,21 +74,28 @@ class Solution {
                 break;
             default: break;
         }
+        // 거리 저장
         int cnt = 0;
+        // 거리 1만큼 움직일 때의 다음 좌표
         int[] next = new int[2];
         while(cnt < dist){
+            // 다음 좌표 설정
             next[0] = temp[0] + dx[dir];
             next[1] = temp[1] + dy[dir];
             // 벽에 걸리는지
             if(next[0]<0 || next[0]>=h || next[1]<0 || next[1] >=w){
-                return cur;
+                temp = cur;
+                break;
             }
             // 장애물에 걸리는 지
             if(park[next[0]].charAt(next[1])=='X'){
-                return cur;
+                temp = cur;
+                break;
             }
+            // 그 어느 조건도 만족하지 않으면 다음 좌표로 이동            
             temp[0] = next[0];
             temp[1] = next[1];
+            // 거리 증가
             cnt++;
         }
         return temp;

8e40c5e5db05c92cc77b183da8aa574fca1b0469
허동민 <131800093+HurDong@users.noreply.github.com>
2025-04-12 02:02:18 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1156 ms, Memory: 206788 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,45 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-    static ArrayList<ArrayList<Integer>> tree = new ArrayList<>();
-    static int[] parents;
-    static boolean[] visited;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        ArrayList<ArrayList<Integer>> tree = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(st.nextToken());
-        parents = new int[N + 1];
-        visited = new boolean[N + 1];
         for (int i = 0; i <= N; i++) {
             tree.add(new ArrayList<>());
         }
         for (int i = 0; i < N - 1; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int a = sc.nextInt();
+            int b = sc.nextInt();
             tree.get(a).add(b);
             tree.get(b).add(a);
         }
-        findParent(1);
+        int[] parents = new int[N + 1];
+        boolean[] visited = new boolean[N + 1];
+        visited[1] = true;
+        dfs(1, tree, parents, visited);
         for (int i = 2; i <= N; i++) {
             sb.append(parents[i] + "\n");
         }
-        System.out.println(sb.toString());
+        System.out.print(sb.toString());
+
     }
 
-    private static void findParent(int node) {
-        visited[node] = true;
-        // 모든 tree의 자식 노드에 대해
+    public static void dfs(int node, ArrayList<ArrayList<Integer>> tree, int[] parents, boolean[] visited) {
         for (int child : tree.get(node)) {
             if (!visited[child]) {
+                visited[child] = true;
                 parents[child] = node;
-                findParent(child);
+                dfs(child, tree, parents, visited);
             }
         }
     }

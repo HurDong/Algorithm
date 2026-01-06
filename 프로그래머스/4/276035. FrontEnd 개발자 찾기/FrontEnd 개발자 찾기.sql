ca55e4d4a11d9c153c8669e1c8ddefa51ad073b6
허동민 <131800093+HurDong@users.noreply.github.com>
2026-01-07 04:03:43 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,2 @@
+-- 코드를 작성해주세요
+select distinct d.id, d.email, d.first_name, d.last_name from developers d join skillcodes s on (s.code & d.skill_code) > 0 and s.category = 'Front End' order by d.id asc;
